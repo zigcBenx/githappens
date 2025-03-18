ebe9ce1512ea55a4db3b28d9d8306d17af22869f
Dore Pavlic <dore.pavlic@dewesoft.com>
2025-03-18 08:55:43 +0100

    handle non existant key in config

--- gitHappens.py
+++ gitHappens.py
@@ -23,7 +23,7 @@ GROUP_ID        = config.get('DEFAULT', 'group_id')
 CUSTOM_TEMPLATE = config.get('DEFAULT', 'custom_template')
 GITLAB_TOKEN    = config.get('DEFAULT', 'GITLAB_TOKEN')
 DELETE_BRANCH   = config.get('DEFAULT', 'delete_branch_after_merge').lower() == 'true'
-DEVELOPER_USERNAME   = config.get('DEFAULT', 'developer_username')
+DEVELOPER_EMAIL   = config.get('DEFAULT', 'developer_email', fallback=None)
 SQUASH_COMMITS  = config.get('DEFAULT', 'squash_commits').lower() == 'true'
 MAIN_BRANCH     = 'master'
 
@@ -364,8 +364,8 @@ def get_two_weeks_commits():
     two_weeks_ago = (datetime.datetime.now() - datetime.timedelta(weeks=2)).strftime('%Y-%m-%d')
 
     cmd = f'git log --since={two_weeks_ago} --format="%ad - %ae - %s" --date=short | grep -v "Merge branch"'
-    if (DEVELOPER_USERNAME):
-        cmd = f'{cmd} | grep {DEVELOPER_USERNAME}'
+    if (DEVELOPER_EMAIL):
+        cmd = f'{cmd} | grep {DEVELOPER_EMAIL}'
 
     try:
         output = subprocess.check_output(cmd, shell=True, text=True, stderr=subprocess.DEVNULL, universal_newlines=True).strip()
